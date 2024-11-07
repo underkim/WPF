b3ab9f3c7cedcc516756b50525f578dcc0d5ad35
underkim <rlaehdgns4568@gmail.com>
2024-11-07 11:30:02 +0900

    다크 윈도우 설정과 다크 스크롤뷰어 설정

---
+++ WpfExplorer.Support/WpfExplorer.Support/UI/Units/DarkWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using Jamesnet.Wpf.Controls;
+
+namespace WpfExplorer.Support.UI.Units
+{
+    public class DarkWindow : JamesWindow
+    {
+        static DarkWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DarkWindow),
+                new FrameworkPropertyMetadata(typeof(DarkWindow)));
+        }
+    }
+}
